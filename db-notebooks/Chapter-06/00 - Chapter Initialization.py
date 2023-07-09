f150c6fd0929b3cf1ce8748b70aed62b26226dcf
Dannyjaydavis@gmail.com <Dannyjaydavis@gmail.com>
2023-07-09 22:33:07 +0000

    notebooks for chapter 5 and chapter 6
    committing notebooks and some edits for chapter 5 and 6

--- db-notebooks/Chapter-06/00 - Chapter Initialization.py
+++ db-notebooks/Chapter-06/00 - Chapter Initialization.py
@@ -2,20 +2,21 @@
 # MAGIC %md-sandbox
 # MAGIC <img src= "https://cdn.oreillystatic.com/images/sitewide-headers/oreilly_logo_mark_red.svg"/>&nbsp;&nbsp;<font size="16"><b>Delta Lake: Up and Running<b></font></span>
 # MAGIC <img style="float: left; margin: 0px 15px 15px 0px;" src="https://learning.oreilly.com/covers/urn:orm:book:9781098139711/400w/" />  
-# MAGIC 
+# MAGIC
 # MAGIC  
 # MAGIC   Name:          chapter 06/00 - Chapter 6 Initialization
-# MAGIC 
+# MAGIC
 # MAGIC     Purpose:  The notebooks in this folder contains the code for chapter 6 of the book - Using Time Travel.
 # MAGIC               This notebook resets and sets up all Hive databases and data files, so that we can successfully 
 # MAGIC               execute all notebooks in this chapter in sequence.
-# MAGIC 
+# MAGIC
 # MAGIC                 
 # MAGIC     The following actions are taken in this notebook:
 # MAGIC      1 - Drop the taxidb database with a cascade, deleting all tables in the database
-# MAGIC      2 - Copy the YellowTaxisParquet files from DataFiles to the chapter05 directory
-# MAGIC      3 - Read the parquet files, and write the table in Delta Format
+# MAGIC      2 - Copy the YellowTaxisParquet files from DataFiles to the chapter06 directory
+# MAGIC      3 - Read the parquet files, and write the Delta table
 # MAGIC      4 - Create database and register the delta table in hive
+# MAGIC      5 - Create a Delta table using a dataframe with custom taxi data
 # MAGIC    
 
 # COMMAND ----------
@@ -25,7 +26,7 @@
 
 # COMMAND ----------
 
-# DBTITLE 0,Drop the taxidb database and all of its tables
+# DBTITLE 1,Drop the taxidb database and all of its tables
 # MAGIC %sql
 # MAGIC DROP DATABASE IF EXISTS taxidb CASCADE
 
@@ -36,26 +37,36 @@
 
 # COMMAND ----------
 
+# DBTITLE 1,Remove existing parquet files
 # MAGIC %fs
 # MAGIC rm -r /mnt/datalake/book/chapter06/YellowTaxisParquet
 
 # COMMAND ----------
 
+# DBTITLE 1,Remove existing delta table
+# MAGIC %fs
+# MAGIC rm -r /mnt/datalake/book/chapter06/TripAggregatesDelta
+
+# COMMAND ----------
+
+# DBTITLE 1,Remove existing delta table
+# MAGIC %fs
+# MAGIC rm -r /mnt/datalake/book/chapter06/YellowTaxisDelta
+
+# COMMAND ----------
+
+# DBTITLE 1,Copy parquet files to this chapter
 # MAGIC %fs
 # MAGIC cp -r mnt/datalake/book/DataFiles/YellowTaxisParquet /mnt/datalake/book/chapter06/YellowTaxisParquet
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ###3 - Read the parquet files, and write the table in Delta Format
+# MAGIC ###3 - Read the parquet files, and write to a Delta table
 
 # COMMAND ----------
 
-# MAGIC %fs
-# MAGIC rm -r /mnt/datalake/book/chapter06/YellowTaxisDelta
-
-# COMMAND ----------
-
+# DBTITLE 1,Read files and write to Delta table
 df = spark.read.format("parquet").load("/mnt/datalake/book/chapter06/YellowTaxisParquet/2022")
 df.write.format("delta").mode("overwrite").save("/mnt/datalake/book/chapter06/YellowTaxisDelta/")
 
@@ -66,28 +77,39 @@ df.write.format("delta").mode("overwrite").save("/mnt/datalake/book/chapter06/Ye
 
 # COMMAND ----------
 
+# DBTITLE 1,Create database
 # MAGIC %sql
 # MAGIC CREATE DATABASE IF NOT EXISTS taxidb;
 
 # COMMAND ----------
 
+# DBTITLE 1,Register Delta table in Hive
 # MAGIC %sql
 # MAGIC CREATE TABLE IF NOT EXISTS taxidb.tripData
 # MAGIC USING DELTA LOCATION '/mnt/datalake/book/chapter06/YellowTaxisDelta';
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC select * from taxidb.tripData
+# MAGIC %md
+# MAGIC ###5 - Create aggregate delta table to demonstrate CDF
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC --set log retention to 365 days
-# MAGIC ALTER TABLE taxidb.tripData SET TBLPROPERTIES(delta.logRetentionDuration = "interval 365 days");
-# MAGIC 
-# MAGIC --set data file retention to 365 days
-# MAGIC ALTER TABLE taxidb.tripData SET TBLPROPERTIES(delta.deletedFileRetentionDuration = "interval 365 days");
-# MAGIC 
-# MAGIC --show the table properties to confirm data and log file retention
-# MAGIC SHOW TBLPROPERTIES taxidb.tripData;
+# DBTITLE 1,Create Aggregate Delta table
+from pyspark.sql.types import StructType,StructField, IntegerType
+
+# specify data and schema for the dataframe
+data = [(1, 1000, 2000), (2, 100, 1500), (3, 7000, 10000), (4, 500, 700)]
+columns = StructType([ 
+                 StructField("VendorId", IntegerType(), True),
+                 StructField("PassengerCount", IntegerType(), True),
+                 StructField("FareAmount", IntegerType(), True),
+             ])
+
+# create delta table
+spark.createDataFrame(data=data, schema = columns)\
+.write\
+.format("delta")\
+.option("delta.enableChangeDataFeed", "true")\
+.mode("overwrite")\
+.save("/mnt/datalake/book/chapter06/TripAggregatesDelta/")
--- db-notebooks/Chapter-06/01 - Time Travel.py
+++ db-notebooks/Chapter-06/01 - Time Travel.py
@@ -2,13 +2,13 @@
 # MAGIC %md-sandbox
 # MAGIC <img src= "https://cdn.oreillystatic.com/images/sitewide-headers/oreilly_logo_mark_red.svg"/>&nbsp;&nbsp;<font size="16"><b>Delta Lake: Up and Running<b></font></span>
 # MAGIC <img style="float: left; margin: 0px 15px 15px 0px;" src="https://learning.oreilly.com/covers/urn:orm:book:9781098139711/400w/" />  
-# MAGIC 
+# MAGIC
 # MAGIC  
 # MAGIC   Name:          chapter 06/01 - Chapter 6 Time Travel
-# MAGIC 
+# MAGIC
 # MAGIC      Purpose:   The notebooks in this folder contains the code for chapter 6 of the book - Using Time Travel.
 # MAGIC                 This notebook illustrates the how compaction works using the repartition method
-# MAGIC 
+# MAGIC
 # MAGIC                 
 # MAGIC      The following actions are taken in this notebook:
 # MAGIC        1 - Compact the existing delta table
@@ -27,11 +27,11 @@
 # MAGIC UPDATE taxidb.tripData
 # MAGIC SET VendorId = 10
 # MAGIC WHERE VendorId = 1;
-# MAGIC 
+# MAGIC
 # MAGIC --update records in table 
 # MAGIC DELETE FROM taxidb.tripData
 # MAGIC WHERE VendorId = 2; 
-# MAGIC 
+# MAGIC
 # MAGIC --describe the table history
 # MAGIC DESCRIBE HISTORY taxidb.tripData
 
@@ -45,10 +45,18 @@
 # MAGIC %sql
 # MAGIC --restore table to previous version
 # MAGIC RESTORE TABLE taxidb.tripData TO VERSION AS OF 0;
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC --describe the table history
 # MAGIC DESCRIBE HISTORY taxidb.tripData;
+
+# COMMAND ----------
+
+# import OS module
+import os
+
+# count files in directory
+print(len(os.listdir('/dbfs/'+ '/mnt/datalake/book/chapter06/YellowTaxisDelta')))
 
 # COMMAND ----------
 
